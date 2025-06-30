3b6287f555d4988d0673847cc6716e1c27048585
Axel David González Molina <3563080220101@ingenieria.usac.edu.gt>
2025-06-29 22:41:54 -0600

    proyecto 2 casi terminado

---
+++ test/Prueba.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Myprogram {
+    static void Main (string[] args){
+
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+
+    }
+}
